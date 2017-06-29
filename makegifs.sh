a663a5ddaf18c7799cb5a83a70a40ce90b87af17
Yakkala Yagnesh Raghava <hi@yagnesh.org>
2017-06-29 16:30:41 +0530

    * makegifs.sh: copy paste errors

--- makegifs.sh
+++ makegifs.sh
@@ -3,13 +3,14 @@
 
 # imd
 imddir=/data/imd/
-l10=`cd $imddir; ls -1t | head -10`
+l10=`cd $imddir; ls -1t *jpg | head -10`
 
 cd $imddir;
 convert -delay 50 -loop 0 $l10 3Dglobe.gif
 
 # wisc
-wiscdir=/data/wisc
-l8=`cd $imddir; ls -1t | head -8`
-convert -delay 50 -loop 0 $l8 3Dglobe.gif
+wiscdir=/data/wisc/
+l8=`cd $wiscdir; ls -1t *.jpg | head -8`
 
+cd $wiscdir;
+convert -delay 50 -loop 0 $l8 xxirm5bbm.gif
