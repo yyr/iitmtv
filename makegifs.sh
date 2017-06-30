3169850f9d2d8a4f5989fea95b138d9463768127
Yakkala Yagnesh Raghava <hi@yagnesh.org>
2017-06-30 09:39:52 +0530

    * makegifs.sh: reverse the gif order.

--- makegifs.sh
+++ makegifs.sh
@@ -3,14 +3,14 @@
 
 # imd
 imddir=/data/imd/
-l10=`cd $imddir; ls -1t *jpg | head -10`
+l10=`cd $imddir; ls -1t *jpg | head -10| tac`
 
 cd $imddir;
 convert -delay 50 -loop 0 $l10 3Dglobe.gif
 
 # wisc
 wiscdir=/data/wisc/
-l8=`cd $wiscdir; ls -1t *.jpg | head -8`
+l8=`cd $wiscdir; ls -1t *.jpg | head -8 | tac`
 
 cd $wiscdir;
 convert -delay 50 -loop 0 $l8 xxirm5bbm.gif
