09c3b5db41df6ae9af32ab31d238fa63f3aee738
Yakkala Yagnesh Raghava <hi@yagnesh.org>
2017-07-04 11:23:39 +0530

    bring other two scripts similar to wisc_down.py

--- imd_sat.py
+++ imd_sat.py
@@ -9,30 +9,34 @@ tmp_file="3Dglobe_ir1_index.html"
 
 old_time = 0
 data_dir = "/data/imd/"
+remote_imgname = "3Dglobe_ir1.jpg"
+log_file = "/var/log/imd_sat.log"
 
-
-def imd_down():
-    remote_imgname = "3Dglobe_ir1.jpg"
+def imd_down(url_base, url, tmp_file, data_dir, remote_imgname):
+    global old_time, log_file
 
     base = down_page(url_base, tmp_file)
     page = get_page(tmp_file)
-    new_time = get_epo_from_dir_page(page,remote_imgname)
+    time_stamp = get_time_from_dir_page(page,remote_imgname)
+    new_time = get_epo_from_dir_page(time_stamp)
 
+    t = time.strftime('%Y-%m-%d-%H:%M:%S')
     if check_new(old_time, new_time):
+        old_time = new_time
         print("new image available")
-        globals()['old_time'] = new_time
-        t = time.strftime('%Y-%m-%d-%H:%M:%S')
-        file_name="3Dglobe_ir1_%s.jpg" % t
+        file_name="3Dglobe_ir1_%s.jpg" % new_time
 
         with cd(data_dir):
             get_image(url, file_name)
+            with open(log_file, "ac") as logfile:
+                logfile.write("{0} image is downloaded at {1} \n".format( time_stamp, t))
 
     else:
-        print("No new image available")
+        print("%s: No new image is available" % t)
 
     return
 
 if __name__ == '__main__':
     while True:
-        imd_down()
+        imd_down(url_base, url, tmp_file, data_dir, remote_imgname)
         time.sleep(1200)
