d28487f147ab52d6712385dd054f4121a7440eb9
Yakkala Yagnesh Raghava <hi@yagnesh.org>
2017-07-04 10:49:09 +0530

    * makegifs.sh: more images.

--- makegifs.sh
+++ makegifs.sh
@@ -1,16 +1,25 @@
 #!/bin/bash
 # Created: Thursday, June 29 2017
 
-# imd
-imddir=/data/imd/
-l10=`cd $imddir; ls -1t *jpg | head -10| tac`
-
-cd $imddir;
-convert -delay 50 -loop 0 $l10 3Dglobe.gif
-
 # wisc
 wiscdir=/data/wisc/
-l8=`cd $wiscdir; ls -1t *.jpg | head -8 | tac`
-
 cd $wiscdir;
+l8=`ls -1t *.jpg | head -8 | tac`
+
+sleep 2
+echo convert -delay 50 -loop 0 $l8 xxirm5bbm.gif
 convert -delay 50 -loop 0 $l8 xxirm5bbm.gif
+
+# imd
+imddir=/data/imd/
+cd $imddir;
+l20=`ls -1t 3Dg*jpg | head -10| tac`
+
+sleep 2
+echo convert -delay 50 -loop 0 $l20 3Dglobe.gif
+convert -delay 50 -loop 0 $l20 3Dglobe.gif
+
+l20=`ls -1t 3DR*jpg | head -10| tac`
+sleep 2
+echo convert -delay 50 -loop 0 $l20 3DRhalfhr_olr.gif
+convert -delay 50 -loop 0 $l20 3DRhalfhr_olr.gif
