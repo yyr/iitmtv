548560128ce5b6f5746badf27c919d92822423e8
Yakkala Yagnesh Raghava <hi@yagnesh.org>
2017-07-07 15:52:01 +0530

    * imd_gfs.py: more forecast files from imd site.

--- imd_gfs.py
+++ imd_gfs.py
@@ -14,7 +14,18 @@ files = ["00hgfs_850wind.gif",
          "168hgfs_850wind.gif",
          "192hgfs_850wind.gif",
          "216hgfs_850wind.gif",
-         "240hgfs_850wind.gif"]
+         "240hgfs_850wind.gif",
+         "00hGFS1534rain.gif",
+         "24hGFS1534rain.gif",
+         "48hGFS1534rain.gif",
+         "72hGFS1534rain.gif",
+         "96hGFS1534rain.gif",
+         "120hGFS1534rain.gif",
+         "144hGFS1534rain.gif",
+         "168hGFS1534rain.gif",
+         "192hGFS1534rain.gif",
+         "216hGFS1534rain.gif",
+         "240hGFS1534rain.gif"]
 
 data_dir = "/data/imd/gfs/"
 log_file = "/var/log/imd_gfs.log"
@@ -23,10 +34,10 @@ def gfs_down(url_base, files, data_dir):
     for f in files:
         with cd(data_dir):
             get_image(url_base + f, f)
+            print("%s  downloaded" % f)
 
     with open(log_file, "ac") as logfile:
         logfile.write("gfs forecast data downloaded ")
-
 
     return
 
