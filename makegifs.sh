4208ee00965b5ecd5df0ebc805e9e3b7dd857a0d
Yakkala Yagnesh Raghava <hi@yagnesh.org>
2017-07-12 16:58:58 +0530

    * makegifs.sh: gfs stich and animate.

--- makegifs.sh
+++ makegifs.sh
@@ -32,26 +32,23 @@ function makegif()
         gfs )
             gfsdir=/data/imd/gfs/
             cd $gfsdir
-            echo convert -delay 100 -loop 0 00hgfs_850wind.gif 24hgfs_850wind.gif 48hgfs_850wind.gif\
-                 72hgfs_850wind.gif 96hgfs_850wind.gif 120hgfs_850wind.gif 144hgfs_850wind.gif\
-                 168hgfs_850wind.gif 192hgfs_850wind.gif 216hgfs_850wind.gif 240hgfs_850wind.gif\
-                 850wind.gif
 
-            convert -delay 100 -loop 0 00hgfs_850wind.gif 24hgfs_850wind.gif 48hgfs_850wind.gif\
-                    72hgfs_850wind.gif 96hgfs_850wind.gif 120hgfs_850wind.gif 144hgfs_850wind.gif\
-                    168hgfs_850wind.gif 192hgfs_850wind.gif 216hgfs_850wind.gif 240hgfs_850wind.gif\
-                    850wind.gif
+            # montage 00hgfs_850wind.gif    00hGFS1534rain.gif -tile 2x1 -geometry +0+0  00h_stiched.gif
+            montage 24hgfs_850wind.gif    24hGFS1534rain.gif -tile 2x1 -geometry +0+0  24h_stiched.gif
+            montage 48hgfs_850wind.gif    48hGFS1534rain.gif -tile 2x1 -geometry +0+0  48h_stiched.gif
+            montage 72hgfs_850wind.gif    72hGFS1534rain.gif -tile 2x1 -geometry +0+0  72h_stiched.gif
+            montage 96hgfs_850wind.gif    96hGFS1534rain.gif -tile 2x1 -geometry +0+0  96h_stiched.gif
+            montage 120hgfs_850wind.gif  120hGFS1534rain.gif -tile 2x1 -geometry +0+0 120h_stiched.gif
+            montage 144hgfs_850wind.gif  144hGFS1534rain.gif -tile 2x1 -geometry +0+0 144h_stiched.gif
+            montage 168hgfs_850wind.gif  168hGFS1534rain.gif -tile 2x1 -geometry +0+0 168h_stiched.gif
+            montage 192hgfs_850wind.gif  192hGFS1534rain.gif -tile 2x1 -geometry +0+0 192h_stiched.gif
+            montage 216hgfs_850wind.gif  216hGFS1534rain.gif -tile 2x1 -geometry +0+0 216h_stiched.gif
+            montage 240hgfs_850wind.gif  240hGFS1534rain.gif -tile 2x1 -geometry +0+0 240h_stiched.gif
 
+            convert -delay 100 -loop 0 24h_stiched.gif 48h_stiched.gif 72h_stiched.gif \
+                    96h_stiched.gif 120h_stiched.gif 144h_stiched.gif 168h_stiched.gif \
+                    192h_stiched.gif 216h_stiched.gif 240h_stiched.gif rain.gif
 
-            echo convert -delay 100 -loop 0 00hGFS1534rain.gif 24hGFS1534rain.gif 48hGFS1534rain.gif\
-                 72hGFS1534rain.gif 96hGFS1534rain.gif 120hGFS1534rain.gif 144hGFS1534rain.gif\
-                 168hGFS1534rain.gif 192hGFS1534rain.gif 216hGFS1534rain.gif 240hGFS1534rain.gif\
-                 rain.gif
-
-            convert -delay 100 -loop 0 00hGFS1534rain.gif 24hGFS1534rain.gif 48hGFS1534rain.gif\
-                    72hGFS1534rain.gif 96hGFS1534rain.gif 120hGFS1534rain.gif 144hGFS1534rain.gif\
-                    168hGFS1534rain.gif 192hGFS1534rain.gif 216hGFS1534rain.gif 240hGFS1534rain.gif\
-                    rain.gif
             ;;
     esac
 }
